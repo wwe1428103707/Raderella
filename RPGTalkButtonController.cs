d6dd9cef10d1c245da9cf4cd84ab6b2ac5df3517
wwe1428103707 <2410712588@qq.com>
2018-01-16 17:12:23 +0800

    Dialog V2.0
    放弃XML文件
    改用Unity商店免费插件RpgTalk
    Downald Site:
    https://www.assetstore.unity3d.com/#!/content/73392
    Introduction Site:
    http://www.seizestudios.com/developer/rpgtalk/

---
+++ RPGTalkButtonController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RPGTALK.Localization;
+//using RPGTALK;
+using System.Collections;
+
+public class RPGTalkButtonController : MonoBehaviour {
+
+    public RPGTalk rpgtalk;
+
+	// Use this for initialization
+	void Start () {
+        rpgtalk.OnMadeChoice += OnMadeChoice;
+	}
+
+    void OnMadeChoice(int questionId, int choiceID)
+    {
+        if (choiceID == 0 && questionId == 0)
+        {
+            rpgtalk.NewTalk("question_0_start", "question_0_end");
+            Debug.Log("SAN值减1");
+        }
+    }
+}
